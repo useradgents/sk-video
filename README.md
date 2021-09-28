ffe7fb22a2d3192172198855a3aa7e358ce36c84
Scotet <mscotet.lmit@gmail.com>
2021-09-28 09:13:08 +0200

    Documentation WIP

--- README.md
+++ README.md
@@ -1,6 +1,31 @@
-SKVideo
+# SKVideo
 un composant Skot pour afficher/contrôler une vidéo
-Basé sur ExoPlayer
+Pour l'instant seule la version android "legacy" est disponible
+
+Elle est basée sur ExoPlayer
+
+## Utilisation
+
+Pour utiliser la librairie vous devez ajouter la ligne suivante à votre fichier _skot_librairies.properties_
+
+`com.github.MathieuScotet.sk-video:0.0.1-alpha45,tech.skot.libraries.video.di.skvideoModule`
+
+Cette libririe consiste en un seul SKComponent:
+
+
+son ViewContract : [SKVideoVC](/documentation/gfm/viewcontract/viewcontract/tech.skot.libraries.video/-s-k-video-v-c/index.md) 
+
+son ViewModel (open) : [SKVideoVM](/documentation/gfm/viewmodel/viewmodel/tech.skot.libraries.video/-s-k-video/index.md) 
+
+
+Attention son comportement est le suivant :
+
+il faut appeler les méthodes onPause et onResume sur les onPause et onResume du SKScreen parent du composant.
+
+La lecture de la vidéo dépend à la fois d'une propriété `playing` et du fait que l'écran soit resumed ou non
 
 
 [documentation](documentation/gfm/index.md)
+
+
+
--- viewcontract/src/commonMain/kotlin/tech/skot/libraries/video/SKVideoVC.kt
+++ viewcontract/src/commonMain/kotlin/tech/skot/libraries/video/SKVideoVC.kt
@@ -3,12 +3,37 @@ package tech.skot.libraries.video
 import tech.skot.core.components.SKComponentVC
 import tech.skot.core.components.SKLayoutIsSimpleView
 
+/**
+ * Un composant permettant d'afficher et de contrôler une Video
+ * lue depuis une url fixe
+ *
+ *
+ * Les méthodes onPause et onResume doivent être appelées par le ViewModel lors
+ * sur les onPause et onResume du SKScreen contenant le composant
+ *
+ * 
+ * La vidéo est lue si le composant est "resumed" et si la propriété 'playing' est à true
+ *
+ * @property url chaîne fixe donnant l'url de la vidéo à lire
+ * @property playing Booléen indiquant si la vidéo doit être jouée ou non
+ * @property playing Booléen indiquant si le son doit être joué ou non
+ *
+ *
+ *
+ */
 @SKLayoutIsSimpleView
 interface SKVideoVC: SKComponentVC {
     val url:String
     var playing:Boolean
     var sound:Boolean
 
+    /**
+     * méthode à appelé sur le onPause de l'écran contenant
+     */
     fun onPause()
+
+    /**
+     * méthode à appelé sur le onResume de l'écran contenant
+     */
     fun onResume()
 }
