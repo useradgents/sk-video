1ee5474206a72e3094536c24f85cc09e6a0f1427
sgueniot <s.gueniot@useradgents.com>
2023-03-21 19:16:09 +0100

    SKVideo -> fix video cache, set cache in folder

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val group = "tech.skot.libraries.sk-video"
-    const val library = "0.2.3"
+    const val library = "0.2.4"
     const val frameworkGroup = "tech.skot"
-    const val framework = "1.2.0"
+    const val framework = "1.2.7"
 }
