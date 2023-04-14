585d135354e3432d75264456225a7e432c0e9ff3
sgueniot <s.gueniot@useradgents.com>
2023-04-14 16:32:43 +0200

    [FEAT] local file

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
     const val group = "com.github.useradgents.sk-video"
-    const val library = "0.2.7"
+    const val library = "0.2.8"
     const val frameworkGroup = "com.github.useradgents.skot"
     const val framework = "1.2.9-ua"
 }
